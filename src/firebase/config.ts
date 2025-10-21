b459500f0dec8385dd67e2419d62d20d7601f901
VNDR <support@vndrmusic.com>
2025-10-21 12:34:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "autopulse-connect",
+  "appId": "1:109075897024:web:5d849407a91b6713b36b25",
+  "apiKey": "AIzaSyBt6iMZnimukbpxURalarZs-AGt-CF4Oig",
+  "authDomain": "autopulse-connect.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "109075897024"
+};
