0402344728b761292f2b1b8ad337fa62537ece80
VNDR <support@vndrmusic.com>
2025-10-21 03:05:47 +0000

    Whitelist all images

--- next.config.ts
+++ next.config.ts
@@ -13,27 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
